3ced48e6f87f548e023c73758885bbd746078626
Segelzwerg <marcel.haas@hhu.de>
2021-01-07 23:00:34 +0100

    ADD: base test case

---
+++ test/BaseTestCase.py
@@ -0,0 +1,23 @@
+import os
+from shutil import rmtree
+
+from flask_testing import TestCase
+
+import brain_server
+
+
+class BaseTestCase(TestCase):
+    """
+    Basic Test Case that setups the flask app.
+    """
+    def create_app(self):
+        instance_path = os.path.join(os.path.dirname(__file__), 'instance')
+
+        app = brain_server.create_app({
+            'TESTING': True,
+            'WTF_CSRF_ENABLED': False
+        }, instance_path)
+        return app
+
+    def tearDown(self) -> None:
+        rmtree(self.app.instance_path)
