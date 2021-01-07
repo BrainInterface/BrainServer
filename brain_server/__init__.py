f1b64142993f211ed34b7cda9f73e006e79238cc
Segelzwerg <marcel.haas@hhu.de>
2021-01-07 23:25:19 +0100

    ADD: secret key to mapping

--- brain_server/__init__.py
+++ brain_server/__init__.py
@@ -7,6 +7,9 @@ from flask_api import FlaskAPI
 def create_app(test_config: dict[str, Any] = None, instance_path: str = None) -> FlaskAPI:
 
     app = FlaskAPI(__name__, instance_relative_config=True, instance_path=instance_path)
+    app.config.from_mapping(
+        SECRET_KEY=os.environ.get('SECRET_KEY') or 'very-secret-key'
+    )
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
