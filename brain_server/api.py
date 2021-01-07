3825173d72609bcdf41416a07efce7eca996d506
Segelzwerg <marcel.haas@hhu.de>
2021-01-07 23:37:56 +0100

    UPDATE: return actions

--- brain_server/api.py
+++ brain_server/api.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request
-from flask_api.status import HTTP_400_BAD_REQUEST
+from flask_api.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
+
+from services.action_service import ActionService
 
 api = Blueprint('api', __name__)
 
@@ -14,4 +16,5 @@ def decision_request():
     observations = request.form.getlist('obs')
     if observations is None or len(observations) == 0:
         return {'error': 'No Observation sent.'}, HTTP_400_BAD_REQUEST
-    return {'action': '1'}
+    service = ActionService(observations)
+    return {'action': service.get_actions()}, HTTP_200_OK
