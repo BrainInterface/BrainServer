809ffae71ddd7d59f06af36a95fb5d139a7a521f
Segelzwerg <marcel.haas@hhu.de>
2021-01-08 21:22:21 +0100

    FIX: imports

--- tests/test_decision_requester.py
+++ tests/test_decision_requester.py
@@ -1,21 +1,22 @@
 from unittest.mock import patch, Mock
 
-from tests.BaseTestCase import BaseTestCase
+from flask_api import status
 
-from flask_api.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
+from tests.BaseTestCase import BaseTestCase
 
 
 class DecisionTest(BaseTestCase):
     """
     Tests the process of requesting a decision via sending observations.
     """
+
     def test_obs_required(self):
         """
         Tests if an error is returned if no observation are sent.
         """
         with self.client:
             response = self.client.post('/decision', data=[])
-            self.assertEqual(HTTP_400_BAD_REQUEST, response.status_code)
+            self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
 
     @patch('services.action_service.ActionService.get_actions', Mock(return_value=['left']))
     def test_action_is_returned(self):
@@ -26,5 +27,5 @@ class DecisionTest(BaseTestCase):
         with self.client:
             data = dict(obs={'color': 1})
             response = self.client.post('/decision', data=data)
-            self.assertEqual(HTTP_200_OK, response.status_code)
+            self.assertEqual(status.HTTP_200_OK, response.status_code)
             self.assertEqual(['left'], response.json['action'])
