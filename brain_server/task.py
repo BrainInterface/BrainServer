b6a4c27d52aa9498b849273ac31c6cca7f21b5b6
Segelzwerg <marcel.haas@hhu.de>
2021-01-15 22:34:31 +0100

    ADD: getter for results

--- brain_server/task.py
+++ brain_server/task.py
@@ -1,2 +1,25 @@
-# pylint: disable=unused-import
-from brain_server import celery
+from typing import Any, Dict
+
+from celery.result import AsyncResult
+
+from brain_server.celery_worker import celery
+
+
+def get_result(task_id: str) -> AsyncResult:
+    """
+    Returns the async result of the task.
+    :param task_id: Is the id of the task for which the result is requested.
+    :return: The AsyncResult object.
+    """
+    return AsyncResult(id=task_id)
+
+
+@celery.task()
+def send_observation(observations: Dict[str, Any]) -> str:
+    """
+    Sends observation to the neuronal network and returns the id for the task.
+    :param observations: Dictionary containing string keys and any type of values.
+    them.
+    :return: The hash id of the task which is a string.
+    """
+    pass
