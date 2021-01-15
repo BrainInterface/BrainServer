b114d18ebe66a2c9c596ca4be4d16f56de3d4417
Segelzwerg <marcel.haas@hhu.de>
2021-01-16 00:35:17 +0100

    ADD: test for guessing model type

--- tests/test_model_service.py
+++ tests/test_model_service.py
@@ -7,6 +7,9 @@ from tests.BaseTestCase import BaseTestCase
 
 
 class TestModel(torch.nn.Module):
+    """
+    Empty class representing a torch module.
+    """
     pass
 
 
@@ -30,4 +33,13 @@ class ModelServiceTest(BaseTestCase):
         model = TestModel()
         torch.save(model.state_dict(), self.path)
         test_model = load_model(self.path, model_type='pytorch', ModelClass=TestModel)
+        self.assertIsNotNone(model, test_model)
+
+    def test_load_model_with_guessing(self):
+        """
+        Tests if a saved model is loaded corrected without given type.
+        """
+        model = TestModel()
+        torch.save(model.state_dict(), self.path)
+        test_model = load_model(self.path, model_type=None, ModelClass=TestModel)
         self.assertIsNotNone(model, test_model)
