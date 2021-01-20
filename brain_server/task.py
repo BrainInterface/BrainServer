7101243e422caac2702335976dc88b836a29ff7a
Segelzwerg <marcel.haas@hhu.de>
2021-01-20 20:10:44 +0100

    ADD: model id to observation send

--- brain_server/task.py
+++ brain_server/task.py
@@ -15,11 +15,12 @@ def get_result(task_id: str) -> AsyncResult:
 
 
 @celery.task()
-def send_observation(observations: Dict[str, Any]) -> str:
+def send_observation(observations: Dict[str, Any], model_id: str) -> str:
     """
     Sends observation to the neuronal network and returns the id for the task.
     :param observations: Dictionary containing string keys and any type of values.
     them.
+    :param model_id: The hash id of the model. It will be loaded inside the task.
     :return: The hash id of the task which is a string.
     """
     pass
