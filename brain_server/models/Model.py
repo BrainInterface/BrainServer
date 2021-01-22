a1bfc26598062c85272db8f45e750b7f3de5e8f5
Segelzwerg <marcel.haas@hhu.de>
2021-01-22 23:32:38 +0100

    FIX: typo

--- brain_server/models/Model.py
+++ brain_server/models/Model.py
@@ -6,5 +6,5 @@ class Model(db.Model):
     Represents a ML-model.
     """
     id = db.Column(db.Integer, index=True, primary_key=True, unique=True)
-    model_type = db.Column(db.text)
-    path = db.Column(db.text, unique=True)
+    model_type = db.Column(db.Text)
+    path = db.Column(db.Text, unique=True)
