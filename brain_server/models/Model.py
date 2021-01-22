f49b93ced84c39f310d37581b8f285dc6d498d8b
Segelzwerg <marcel.haas@hhu.de>
2021-01-22 22:00:51 +0100

    ADD: a basic ml-model

---
+++ brain_server/models/Model.py
@@ -0,0 +1,9 @@
+from models import db
+
+
+class Model(db.Model):
+    """
+    Represents a ML-model.
+    """
+    id = db.Column(db.Integer, index=True, primary_key=True, unique=True)
+    model_type = db.Column(db.text)
