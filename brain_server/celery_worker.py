d1ebece93cbc13e6c2bae98e3b571f9551f1f463
Segelzwerg <marcel.haas@hhu.de>
2021-01-23 21:41:02 +0100

    ADD: backend to celery worker

--- brain_server/celery_worker.py
+++ brain_server/celery_worker.py
@@ -1,5 +1,6 @@
 from celery import Celery
 
-CELERY_BROKER_RULE='redis://localhost:6379/0'
-CELERY_RESULT_BACKEND='redis//localhost:6379/0'
-celery = Celery('BrainServer', broker=CELERY_BROKER_RULE, include=['brain_server.task'])
+CELERY_BROKER_RULE = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+celery = Celery('BrainServer', broker=CELERY_BROKER_RULE, backend=CELERY_RESULT_BACKEND,
+                include=['brain_server.task'])
