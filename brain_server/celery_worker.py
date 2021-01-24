ad92e01ae1e875df4cf27482ab79ad60c31531b5
Segelzwerg <marcel.haas@hhu.de>
2021-01-24 01:01:08 +0100

    FIX: backend and results

--- brain_server/celery_worker.py
+++ brain_server/celery_worker.py
@@ -1,6 +1,7 @@
 from celery import Celery
 
 CELERY_BROKER_RULE = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
 celery = Celery('BrainServer', broker=CELERY_BROKER_RULE, backend=CELERY_RESULT_BACKEND,
-                include=['brain_server.task'])
+                include=['brain_server.task'], ignore_result=False)
+celery.Task.track_started = True
