cb3b139f9119d016a6900bbd18eea503ea4f9e83
Segelzwerg <marcel.haas@hhu.de>
2021-01-27 22:00:57 +0100

    UPDATE: rework to enable keras evaluation inside celery task

--- brain_server/celery_worker.py
+++ brain_server/celery_worker.py
@@ -1,7 +1,17 @@
-from celery import Celery
+from celery import Celery, Task
+
+from brain_server.models.agent import Agent
+from brain_server.services.model_service import load_model
 
 CELERY_BROKER_RULE = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
 celery = Celery('BrainServer', broker=CELERY_BROKER_RULE, backend=CELERY_RESULT_BACKEND,
-                include=['brain_server.task'], ignore_result=False)
+                include=['brain_server.task'], ignore_result=False,
+                accept_content='application/x-python-serialize')
 celery.Task.track_started = True
+
+
+class AgentTask(Task):
+    def __init__(self, ):
+        model = load_model('instance/', model_type='keras')
+        self._agents = {'1': model}
--- brain_server/task.py
+++ brain_server/task.py
@@ -1,15 +1,15 @@
+from logging import log
 from typing import Any, Dict
 
 from tensorflow import keras
 
-from brain_server import agents
-from brain_server.celery_worker import celery
+from brain_server.celery_worker import celery, AgentTask
 from deep_rl.car_agent import pre_process
 
 
-@celery.task(track_started=True)
+@celery.task(base=AgentTask, track_started=True, serializer='pickle')
 def send_observation(observations: Dict[str, Any],
-                     model_id: str) -> str:
+                     model_id: str):
     """
     Sends observation to the neuronal network and returns the id for the task.
     :param observations: Dictionary containing string keys and any type of values.
@@ -17,11 +17,16 @@ def send_observation(observations: Dict[str, Any],
     :param model_id: the hash id of the model.
     :return: The hash id of the task which is a string.
     """
-    model = agents.get(model_id)
+    log(level=10, msg=send_observation.agents)
+    model = send_observation.agents.get(model_id)
     if isinstance(model, keras.Model):
         obs = list(observations.values())
         inputs = pre_process(obs[0])
-        actions = model(inputs).numpy()
+        log(10, inputs)
+        actions = model(inputs).numpy().flatten().tolist()
+        log(10, actions)
         return actions
     else:
-        ValueError(f'Currently only keras models are supported but was {type(model)}')
+        error_message = f'Currently only keras models are supported but was {type(model)}'
+        log(level=40, msg=error_message)
+        ValueError(error_message)
