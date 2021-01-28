db87e4e9ab525b76f93f85188a34c5e07db85256
Segelzwerg <marcel.haas@hhu.de>
2021-01-28 20:15:55 +0100

    UPDATE: test to new returns values

--- tests/services/test_action_serivce.py
+++ tests/services/test_action_serivce.py
@@ -9,23 +9,26 @@ class ActionServiceTest(BaseTestCase):
     Test the action service.
     """
 
-    @patch('brain_server.task.send_observation.AsyncResult', return_value=Mock(result='2'))
+    @patch('brain_server.task.send_observation.AsyncResult', return_value=Mock(result='2',
+                                                                               status='SUCCESS'))
     def test_get_action_done(self, _):
         """
         Tests if the action returned if the task is done.
         """
-        result = ActionService.get_actions('1')
+        status, result = ActionService.get_actions('1')
         self.assertEqual('2', result)
+        self.assertEqual('SUCCESS', status)
 
     @patch('brain_server.task.send_observation.AsyncResult',
-           return_value=MagicMock(status='PENDING', ready=Mock(
+           return_value=MagicMock(status='STARTED', ready=Mock(
                return_value=False)))
     def test_get_action_not_done(self, _):
         """
         Test if the pending status is returned, if the task is not done yet.
         """
-        result = ActionService.get_actions('1')
-        self.assertEqual('PENDING', result)
+        status, result = ActionService.get_actions('1')
+        self.assertEqual('STARTED', status)
+        self.assertIsNone(result)
 
     def test_request_action(self):
         """
