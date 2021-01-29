6b8819311fd7bc8d1371b60464c2e27add6abf9f
Segelzwerg <marcel.haas@hhu.de>
2021-01-29 21:33:16 +0100

    REMOVE: previous necessary strip of brackets

--- deep_rl/car_agent.py
+++ deep_rl/car_agent.py
@@ -12,7 +12,7 @@ def pre_process(inputs: List[str]) -> np.ndarray:
     """
     obs_values: list = list()
     for observation in inputs:
-        obs_value = np.array(float(observation.lstrip('<').split(',')[0]))
+        obs_value = np.array(float(observation.split(',')[0]))
         obs_values.append(np.expand_dims(obs_value, axis=0))
     return np.array(obs_values)
 
